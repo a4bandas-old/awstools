f29fd6f231584015296473d242e3f8a57467fc37
Juan Schwindt <juan@schwindt.org>
2011-01-21 11:22:01 -0300

    Actualización de proceso de boot

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Continua el boot con el script que se baja de S3 (tiene que ser public)
-/root/scripts/runurl http://s3-eu-west-1.amazonaws.com/boot.otlive.es/boot-extra.sh $HOSTNAME
+/root/scripts/runurl http://s3-eu-west-1.amazonaws.com/boot.otlive.es/boot-extra.sh
 
