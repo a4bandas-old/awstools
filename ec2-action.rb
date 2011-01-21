6ec6ea5b369f2ba0f0b1dfdff36bd2f9e4b71ce7
Juan Schwindt <juan@schwindt.org>
2011-01-21 13:11:02 -0300

    ec2-action.rb puede dejar cantidad definida de servers corriendo

--- ec2-action.rb
+++ ec2-action.rb
@@ -4,21 +4,29 @@ require 'rubygems'
 require 'optparse'
 require 'fog'
 
+action = 'status'
 options = {}
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: #{$0} [-c credential] [action]\n    Valid actions: status, add-frontend, remove-frontend"
+  opts.banner = "Usage: #{$0} [-c credential] [--force-run NUM] [action]\n    Valid actions: status, add-frontend, remove-frontend"
 
   options[:credential] = :otlive
   opts.on( '-c', '--credential CRED', 'Use specific credentials from ~/.fog' ) do |cred|
     options[:credential] = cred.to_sym
   end
+  opts.on( '--force-run NUM', 'Start or stop frontends to run NUM servers') do |num|
+    options[:force_num] = num.to_i
+    action = 'force-run'
+    if options[:force_num] < 2
+      puts "force-run must be >= 2"
+      exit
+    end
+  end
 
 end
 optparse.parse!
 
-action = ARGV[0] || 'status'
-
-if action != 'status' && action != 'add-frontend' && action != 'remove-frontend'
+action = ARGV[0] || action
+unless %w(status add-frontend remove-frontend force-run).include? action
   puts optparse
   exit
 end
@@ -28,6 +36,29 @@ ec2 = Fog::AWS::Compute.new
 
 servers = ec2.servers.all.sort{|a,b| a.tags['role'] <=> b.tags['role'] }
 
+def add_frontend(num, servers)
+  servers.each do |server|
+    if server.tags['role'].match(/^frontend\d+$/) && server.state == 'stopped'
+      puts "Starting '#{server.tags['role']}'"
+      server.start
+      num -= 1
+      break if num == 0
+    end
+  end
+end
+
+def remove_frontend(num, servers)
+  servers.each do |server|
+    if server.tags['role'].match(/^frontend\d+$/) && server.state == 'running'
+      next if server.tags['role'].match(/^frontend[12]$/) 
+      puts "Stopping '#{server.tags['role']}'"
+      server.stop
+      num -= 1
+      break if num == 0
+    end
+  end
+end
+
 case action
 when 'status'
   puts "# Servers for #{options[:credential]}"
@@ -35,21 +66,17 @@ when 'status'
     puts "#{server.tags['role']}, public: #{server.ip_address}, private: #{server.private_ip_address} (#{server.state})"
   end
 when 'add-frontend'
-  servers.each do |server|
-    if server.tags['role'].match(/^frontend\d+$/) && server.state == 'stopped'
-      puts "Starting '#{server.tags['role']}'"
-      server.start
-      break
-    end
-  end
+  add_frontend(1, servers)
 when 'remove-frontend'
-  servers.reverse.each do |server|
-    if server.tags['role'].match(/^frontend\d+$/) && server.state == 'running'
-      next if server.tags['role'].match(/^frontend[12]$/) 
-      puts "Stopping '#{server.tags['role']}'"
-      server.stop
-      break
-    end
+  remove_frontend(1, servers)
+when 'force-run'
+  running = servers.count { |server| server.tags['role'].match(/^frontend\d+$/) && server.state == 'running' }
+  if running < options[:force_num]
+    puts "Now running: #{running}, starting #{options[:force_num] - running}"
+    add_frontend(options[:force_num] - running, servers)
+  elsif running > options[:force_num] 
+    puts "Now running: #{running}, stopping #{running - options[:force_num]}"
+    remove_frontend(running - options[:force_num], servers)
   end
 end
 
