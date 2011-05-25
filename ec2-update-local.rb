a6508f9793da2629f28064777231660d9b6ef756
Luis Mayoral <luis@luismayoral.com>
2011-05-25 14:13:07 +0200

    Fix deprecation ip_address

--- ec2-update-local.rb
+++ ec2-update-local.rb
@@ -66,9 +66,9 @@ def get_running_hosts(show = false)
   hosts = {}
   ec2.servers.all.each do |server|
     if server.state == 'running'
-      hosts[server.tags['role']] = { :public => server.ip_address, :private => server.private_ip_address }
+      hosts[server.tags['role']] = { :public => server.public_ip_address, :private => server.private_ip_address }
       if show
-        puts "host: #{server.tags['role']}, public: #{server.ip_address}, private: #{server.private_ip_address}"
+        puts "host: #{server.tags['role']}, public: #{server.public_ip_address}, private: #{server.private_ip_address}"
       end
     end
   end
